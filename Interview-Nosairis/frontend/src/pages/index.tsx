623ed34021ac39b2bff9b9339b91d0c51774bf31
bocal <bocal@u91z08s03.42kl.edu.my>
2023-07-04 15:09:45 +0800

    Alert page done

--- Interview-Nosairis/frontend/src/pages/index.tsx
+++ Interview-Nosairis/frontend/src/pages/index.tsx
@@ -6,8 +6,10 @@ export default function Home() {
   return (
     <main className="bg-blue-950">
       <Graphs />
-      <hr className="border-2"/>
+      <hr className="border-2" />
       <Alerts />
+      <hr className="border-2" />
+      <Edits />
     </main>
   );
 }
@@ -32,9 +34,12 @@ const Graphs = () => {
 
   const NavigateButton = (props: NavigateButtonProps) => {
     const { forward, onClick } = props;
-  
+
     return (
-      <button className="border-2 border-white rounded-md py-1 px-4" onClick={onClick}>
+      <button
+        className="border-2 border-white rounded-md py-1 px-4"
+        onClick={onClick}
+      >
         {forward ? "Forward Hour" : "Previous Hour"}
       </button>
     );
@@ -42,10 +47,11 @@ const Graphs = () => {
 
   return (
     <div className="h-screen">
+      <h1 className="text-center text-4xl pt-4 pb-2">Graph Chart</h1>
       <div className="py-4 flex flex-col items-center">
         {graphData.map((image, index) => (
           <img
-            className="px-5 max-h-[280px]"
+            className="px-5 max-h-[260px]"
             key={index}
             src={image.src}
             alt={`Image ${index}`}
@@ -57,15 +63,71 @@ const Graphs = () => {
           forward={false}
           onClick={() => handlePageChange(false)}
         />
-        <NavigateButton 
-          forward={true}
-          onClick={() => handlePageChange(true)}
-        />
+        <NavigateButton forward={true} onClick={() => handlePageChange(true)} />
       </div>
     </div>
   );
 };
 
-const Alerts = () => {
-  return (<div className="h-screen">Alert</div>);
+interface AlertData {
+  switch_label: string;
+  time: string;
 }
+
+const Alerts = () => {
+  const [alertData, setAlertData] = useState<AlertData[]>([]);
+
+  useEffect(() => {
+    const getAlerts = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8000/switch_status/alert",
+          {
+            method: "GET",
+          }
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setAlertData(data);
+        } else {
+          console.error("Bad response");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getAlerts();
+  }, []);
+
+  return (
+    <div className="h-screen px-10">
+      <h1 className="text-center text-4xl py-10">Alert Page</h1>
+      <div className="max-h-[750px] overflow-y-auto border-2">
+        <table className="w-full">
+          <thead className="border-2">
+            <tr>
+              <th className="border-2">Index</th>
+              <th className="border-2">Switch Label</th>
+              <th className="border-2">Alert Type</th>
+              <th className="border-2">Time</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {alertData.map((alert, index) => (
+              <tr className="border-2" key={index}>
+                <td className="border-2">{index}</td>
+                <td className="border-2">{alert.switch_label}</td>
+                <td className="border-2">Ping Lost</td>
+                <td className="border-2">{alert.time}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+const Edits = () => {
+  return <div className="h-screen">Edit Page</div>;
+};
